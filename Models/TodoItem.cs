49d6b5b00de86f0125d0f9b3468da00f6e0b0dba
JD Singh <info@fermatsolutions.dev>
2023-04-04 20:24:50 -0500

    Initial commit

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace TodoApp.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public bool IsComplete { get; set; }
+}
